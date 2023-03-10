6ad7fb3b3a8c7dd841fb75daeee81e4810a72955
Raffaele <antoniopapaleo24@gmail.com>
2023-03-10 09:44:57 +0100

    Creazione progetto

---
+++ src/main/java/it/corso/businessCase/BusinessCaseApplication.java
@@ -0,0 +1,13 @@
+package it.corso.businessCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/corso/businessCase/BusinessCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.corso.businessCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
